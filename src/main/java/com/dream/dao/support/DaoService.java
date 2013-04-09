624c71f5d23082c3ef572414c99718feee35792b
pan <chengpan168@126.com>
2013-04-09 22:58:08 +0800

    Change-Id: I9b7e623e94459a1675755719cd421417eaf097ba

--- src/main/java/com/dream/dao/support/DaoService.java
+++ src/main/java/com/dream/dao/support/DaoService.java
@@ -1,9 +1,79 @@
 package com.dream.dao.support;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
 
 public interface DaoService {
+
+	public static final String INSERT = "insert";  
+	  
+    public static final String UPDATE = "update";  
+  
+    public static final String DELETE = "delete";  
+  
+    public static final String DELETE_ID = "deleteById";  
+    
+    public static final String SELECT_ID = "selectById";
+  
+    public static final String SELECT = "select";  
+  
+    public static final String SELECT_ALL = "selectAll";  
+  
+    public static final String SELECTMAP = "selectByMap";  
+  
+    public static final String SELECTSQL = "selectBySql";  
+  
+    public static final String COUNT = "count";  
+    
+  
+	public <T> T selectOne(String statementId);
+
+	public <T> T selectOne(String statementId, Object param);
+
+	public <T> T selectOne(Class<T> entityClass, Serializable id);
+
+	public <T> List<T> selectList(String statementId);
 	
-	public <T> T selectOne(String statementId) ;
+	public <T> List<T> selectList(String statementId , Object param);
+
+	public <T> List<T> selectList(String statementId ,  RowBounds rowBounds);
 	
-	public <T> T query(String sql) ;
+	public <T> List<T> selectList(String statementId, Object parameters, RowBounds rowBounds);
+	
+	public <T> List<T> selectPage(String statementId , Page page) ;
+	
+	public long selectCount(String statementId , Page page) ;
+
+	public <T> T query(String sql);
+	
+/*------------------------------------------------分隔线---------------------------------------------------*/
+	
+	public int insert(String statement);
+
+	public <T> int insert(T o);
+
+	public int insert(String statementId, Object param);
+
+	public int insertAll(String statementId, Collection<? extends Object> params);
+
+	/*------------------------------------------------分隔线---------------------------------------------------*/
+	
+	public <T> int update(T o);
+
+	public int update(String statementId, Object parameters);
+
+	public int updateAll(String statementId, Collection<? extends Object> params);
+
+	/*------------------------------------------------分隔线---------------------------------------------------*/
+	public <T> int deleteById(Class<T> entityClass, Serializable id);
+
+	public <T> int delete(T o);
+
+	public int delete(String statementId, Object param);
+
+	public int deleteAll(String statementId, Collection<? extends Object> params);
+
 }
--- src/main/java/com/dream/dao/support/impl/MyBatisDAOService.java
+++ src/main/java/com/dream/dao/support/impl/MyBatisDAOService.java
@@ -1,10 +1,15 @@
 package com.dream.dao.support.impl;
 
+import java.io.Serializable;
 import java.sql.Connection;
+import java.util.Collection;
+import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.dream.dao.support.DaoService;
+import com.dream.dao.support.Page;
 
 public class MyBatisDAOService implements DaoService{
 
@@ -15,10 +20,148 @@ public class MyBatisDAOService implements DaoService{
 		return sqlSessionTemplate.selectOne(statementId);
 	}
 
+	
 	public <T> T query(String sql) {
 		Connection conn = sqlSessionTemplate.getConnection() ;
 		return null ;
 	}
+
+	@Override
+	public <T> T selectOne(String statementId, Object param) {
+		return sqlSessionTemplate.selectOne(statementId , param );
+	}
+
+
+	@Override
+	public <T> T selectOne(Class<T> entityClass, Serializable id) {
+		return sqlSessionTemplate.selectOne(getStatementId(entityClass, SELECT_ID) , id );
+	}
+
+
+	@Override
+	public <T> List<T> selectList(String statementId) {
+		return sqlSessionTemplate.selectList(statementId);
+	}
+
+	@Override
+	public <T> List<T> selectList(String statementId,
+			Object param) {
+		return sqlSessionTemplate.selectList(statementId , param);
+	}
+
+
+	@Override
+	public <T> List<T> selectList(String statementId,
+			RowBounds rowBounds) {
+		return selectList(statementId , null , rowBounds);
+	}
+
+
+	@Override
+	public <T> List<T> selectList(String statementId,
+			Object param, RowBounds rowBounds) {
+		return sqlSessionTemplate.selectList(statementId , param , rowBounds);
+	}
+
+
+	@Override
+	public <T> List<T> selectPage(String statementId,
+			Page page) {
+		RowBounds rowBounds = null ;
+		if(page!=null){
+			rowBounds = new RowBounds(page.getTotal(), page.getPageCount()) ;
+		}
+		return selectList(statementId , page , rowBounds);
+	}
+
+
+	@Override
+	public long selectCount(String statementId, Page page) {
+		return sqlSessionTemplate.selectOne(statementId, page) ;
+	}
+
+
+	@Override
+	public int insert(String statement) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public <T> int insert(T o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int insert(String statementId, Object param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int insertAll(String statementId, Collection<? extends Object> params) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public <T> int update(T o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int update(String statementId, Object parameters) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int updateAll(String statementId, Collection<? extends Object> params) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public <T> int deleteById(Class<T> entityClass, Serializable id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public <T> int delete(T o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int delete(String statementId, Object param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int deleteAll(String statementId, Collection<? extends Object> params) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public String getStatementId(Class<? extends Object> entityClass , String type) {
+		return entityClass.getSimpleName() + "." + type ;
+	}
+	
 //	----------------------------------分隔线--------------------------------------
 	
 	public SqlSessionTemplate getSqlSessionTemplate() {
