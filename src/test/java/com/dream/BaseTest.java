e5fdb2d08abd25670093512de5f2b14bf83c6f95
chengpan <chengpan168@126.com>
2013-04-09 18:02:08 +0800

    Change-Id: I336e688b6a27e0c8daa5b25af5061574505ff980

--- src/test/java/com/dream/BaseTest.java
+++ src/test/java/com/dream/BaseTest.java
@@ -1,5 +1,14 @@
 package com.dream;
 
-public class BaseTest {
+import org.junit.Test;
 
+public class BaseTest {
+	public static void main(String[] args) {
+		System.out.println("hello") ; 
+	}
+	
+	@Test
+	public void testA(){
+		System.out.println("hello test") ;
+	}
 }
